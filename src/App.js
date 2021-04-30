55aca8fe452ea7864284130e9abc88ff4d9181e7
Eniola Ajayi <ajayieniola16@gmail.com>
2021-04-30 23:27:43 +0100

    refactor

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
-import React, { useMemo, useState } from "react";
+import React from "react";
 import Layout from "./components/Layout/Layout";
-// Import the Slate editor factory.
-import { createEditor } from "slate";
-
-// Import the Slate components
-import { Slate, Editable, withReact } from "slate-react";
+import SyncingEditor from "./components/Layout/SyncingEditor";
 
 const App = () => {
-  // We create state for what we pass into editor
-  const [value, setValue] = useState([
-    {
-      type: "paragraph",
-      children: [{ text: "a paragraph... " }],
-    },
-  ]);
-  // then we create a slate Editor object that won't change across renders
-  const editor = useMemo(() => withReact(createEditor()), []);
-  // Render slate context
-  // then add editable component inside context
   return (
     <Layout>
-      <Slate
-        editor={editor}
-        value={value}
-        onChange={(newValue) => {
-          setValue(newValue);
-        }}
-      >
-        <Editable></Editable>
-      </Slate>
+      <SyncingEditor />
     </Layout>
   );
 };
