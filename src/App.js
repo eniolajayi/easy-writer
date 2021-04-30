eada7bc1b12b8e63cca648c6f5e2c18fc980415e
Eniola Ajayi <ajayieniola16@gmail.com>
2021-04-30 19:29:40 +0100

    import neccesary components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
+// Import the Slate editor factory.
+import { createEditor } from "slate";
+
+// Import the Slate components
+import { Slate, Editable, withReact } from "slate-react";
 
 const App = () => {
   return <div>Sup</div>;
