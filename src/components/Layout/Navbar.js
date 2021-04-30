3c690ebf434ff3c20209fd1a3df2d117c6ae2f10
Eniola Ajayi <ajayieniola16@gmail.com>
2021-04-30 20:40:12 +0100

    add navbar code

---
+++ src/components/Layout/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navbar } from "react-bootstrap";
+
+const AppNavBar = () => {
+  return (
+    <Navbar bg="light" variant="light">
+      <Navbar.Brand href="#">EasyDocs.</Navbar.Brand>
+    </Navbar>
+  );
+};
+
+export default AppNavBar;
