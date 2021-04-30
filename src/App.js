cd4b7ae12ddf028b37bfea00bc60579bad226874
Eniola Ajayi <ajayieniola16@gmail.com>
2021-04-30 19:35:44 +0100

    create slate editor object

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import { createEditor } from "slate";
 import { Slate, Editable, withReact } from "slate-react";
 
 const App = () => {
+  // then we create a slate Editor object that won't change across renders
+  const editor = useMemo(() => {
+    withReact(createEditor());
+  }, []);
+
   return <div>Sup</div>;
 };
 export default App;
