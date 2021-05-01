682a93d65c835125891d3ebf817667432d2b4158
Eniola Ajayi <ajayieniola16@gmail.com>
2021-05-01 12:47:48 +0100

    add route template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
-import SyncingEditor from "./components/Layout/SyncingEditor";
+import SyncingEditor from "./components/SyncingEditor";
 
 const App = () => {
   return (
     <Layout>
+      <BrowserRouter>
+        <Route path="/about"></Route>
+        <Route path="/users"></Route>
+        <Route path="/"></Route>
+      </BrowserRouter>
       <SyncingEditor />
     </Layout>
   );
