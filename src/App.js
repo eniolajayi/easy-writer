74c2300b59e0d20b16cfcf17c5c870db22a75e92
Eniola Ajayi <ajayieniola16@gmail.com>
2021-05-22 13:58:42 +0100

    removed Layout component from app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from "react";
 import { BrowserRouter, Redirect, Route } from "react-router-dom";
 import GroupEditor from "./components/GroupEditor";
-import Layout from "./components/Layout/Layout";
 import { v4 as uuidv4 } from "uuid";
 
 const App = () => {
   return (
-    <Layout>
-      <BrowserRouter>
-        <Route
-          path="/"
-          exact
-          render={() => {
-            return <Redirect to={`/group/${uuidv4()}`} />;
-          }}
-        ></Route>
-        <Route path="/group/:id" component={GroupEditor} />
-      </BrowserRouter>
-    </Layout>
+    <BrowserRouter>
+      <Route
+        path="/"
+        exact
+        render={() => {
+          return <Redirect to={`/group/${uuidv4()}`} />;
+        }}
+      ></Route>
+      <Route path="/group/:id" component={GroupEditor} />
+    </BrowserRouter>
   );
 };
 export default App;
