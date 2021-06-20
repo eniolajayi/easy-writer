d60b12c308dbde37c1d3a5547f075c4080ce5ade
Eniola Ajayi <ajayieniola16@gmail.com>
2021-06-20 19:15:33 +0100

    add note to footer

--- src/components/Layout/BottomBar.js
+++ src/components/Layout/BottomBar.js
@@ -3,7 +3,8 @@ import styled from "styled-components";
 
 const Footer = styled.footer`
   width: 100%;
-  height: 70px;
+  height: auto;
+  min-height: 70px;
   background-color: #fff;
   position: fixed;
   border: 1px dashed #000;
@@ -11,9 +12,29 @@ const Footer = styled.footer`
   left: 0;
   z-index: 99;
   box-shadow: -1px -2px 2px 1px rgb(139 139 139 / 10%);
+  .info {
+    padding: 0.9rem;
+    .icon {
+      font-size: 0.625rem;
+      text-transform: uppercase;
+    }
+    .content {
+    }
+  }
 `;
 function BottomBar() {
-  return <Footer></Footer>;
+  return (
+    <Footer>
+      <div className="info">
+        <span className="icon">Note:</span>
+        <p class="content">
+          This app does not have a strong serve side or storage support yet. It
+          was made for learning purposes only. It may or may not support these
+          features in the future.
+        </p>
+      </div>
+    </Footer>
+  );
 }
 
 export default BottomBar;
