4698e285583befdd5620d3be9cfc4d795a42c156
Eniola Ajayi <ajayieniola16@gmail.com>
2021-06-25 14:36:13 +0100

    make command btn for footer

--- src/components/Layout/BottomBar.js
+++ src/components/Layout/BottomBar.js
@@ -1,18 +1,36 @@
 import React from "react";
 import styled from "styled-components";
+import Icon from "../Icon";
 
 const Footer = styled.footer`
+  max-width: 1440px;
   width: 100%;
   height: auto;
   min-height: 70px;
   background-color: #fff;
   position: fixed;
-  border: 1px dashed #000;
   bottom: 0;
   left: 0;
   z-index: 99;
   box-shadow: -1px -2px 2px 1px rgb(139 139 139 / 10%);
+  .controls {
+    height: 28px;
+    background: #fbfbfb;
+    display: flex;
+    align-items: stretch;
+    justify-content: space-between;
+    .btn {
+      flex: 1 0 auto;
+      width: 40px;
+      height: 40px;
+      svg {
+        width: 16px;
+        height: 16px;
+      }
+    }
+  }
   .info {
+    border: 1px dashed #000;
     padding: 0.9rem;
     .icon {
       font-size: 0.625rem;
@@ -22,9 +40,24 @@ const Footer = styled.footer`
     }
   }
 `;
+
+const ActionButton = ({ children, onClick, ...props }) => {
+  return (
+    <button className={props.className} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
 function BottomBar() {
   return (
     <Footer>
+      <div className="controls">
+        <ActionButton className="btn">
+          <Icon type="bold" />
+        </ActionButton>
+        <ActionButton className="btn">Italics</ActionButton>
+        <ActionButton className="btn">Underline</ActionButton>
+      </div>
       <div className="info">
         <span className="icon">Note:</span>
         <p className="content">
